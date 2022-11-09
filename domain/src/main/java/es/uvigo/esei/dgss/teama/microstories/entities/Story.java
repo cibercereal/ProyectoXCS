5c75c0ac6b7a4366f9f43af475768967ac610179
julio <jspilva17@esei.uvigo.es>
2022-11-09 18:43:42 +0100

    tsk1-3 Added basic structure Entity Story
    Creation of the basic structure of the story entity to be able to work simultaneously between groups 1 and 2

---
+++ domain/src/main/java/es/uvigo/esei/dgss/teama/microstories/entities/Story.java
@@ -0,0 +1,72 @@
+package es.uvigo.esei.dgss.teama.microstories.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+
+public class Story implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private int id;
+
+    private Date date;
+
+    private String title;
+
+    private String content;
+
+    private String genre;
+
+    private String mainTheme;
+
+    private String secondaryTheme;
+
+    private String author;
+
+    public Story() {
+    }
+
+    public Story(int id, Date date, String title, String content, String genre, String mainTheme, String secondaryTheme, String author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.content = content;
+        this.genre = genre;
+        this.mainTheme = mainTheme;
+        this.secondaryTheme = secondaryTheme;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getMainTheme() {
+        return mainTheme;
+    }
+
+    public String getSecondaryTheme() {
+        return secondaryTheme;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
