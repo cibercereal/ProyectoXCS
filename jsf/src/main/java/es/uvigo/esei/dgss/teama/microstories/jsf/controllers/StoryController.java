7373f02664eac34b6ecbbdcc976675aaf87ef1d6
Álvaro Suárez <alvarosuafe@gmail.com>
2022-11-21 17:56:44 +0100

    tsk6 Modifies story controller and index jsf
    Adds call to stiryEJB in the controller to show the last 6 recents
    stories in the index jsf.

--- jsf/src/main/java/es/uvigo/esei/dgss/teama/microstories/jsf/controllers/StoryController.java
+++ jsf/src/main/java/es/uvigo/esei/dgss/teama/microstories/jsf/controllers/StoryController.java
@@ -1,46 +1,35 @@
 package es.uvigo.esei.dgss.teama.microstories.jsf.controllers;
 
+import es.uvigo.esei.dgss.teama.microstories.entities.Story;
+import es.uvigo.esei.dgss.teama.microstories.service.StoryEJB;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 @Named(value = "storyController")
 @SessionScoped
 public class StoryController implements Serializable {
-  private List<String> recentStories;
+
+  @EJB
+  private StoryEJB storyEJB;
 
 
   public StoryController() {
 
   }
 
-
-  public List<String> getRecentStories() {
-
-    return recentStories;
-  }
-
-  public void setRecentStories(List<String> recentStories) {
-    this.recentStories = recentStories;
-  }
-
   @PostConstruct
   public void init() {
-
-    recentStories = new ArrayList<>();
-    recentStories.add("Story 1");
-    recentStories.add("Story 2");
-    recentStories.add("Story 3");
-    recentStories.add("Story 4");
-    recentStories.add("Story 5");
-    recentStories.add("Story 6");
-
   }
 
-
+  public List<Story> getRecentStories() {
+    return storyEJB.getRecentStories();
+  }
 
 }
 
