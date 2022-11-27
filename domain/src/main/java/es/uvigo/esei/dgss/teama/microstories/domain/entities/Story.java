0f40bae54d1d58610c4385fb12c5aca4a6222253
Álvaro Suárez <alvarosuafe@gmail.com>
2022-11-27 12:03:27 +0100

    tsk20, tsk23 Adds tests to Story entity and new exception
    Adds new test to entity story to check null and empty values. Also, adds tests
    to checks length not greater than limit on author and content variables.

--- domain/src/main/java/es/uvigo/esei/dgss/teama/microstories/domain/entities/Story.java
+++ domain/src/main/java/es/uvigo/esei/dgss/teama/microstories/domain/entities/Story.java
@@ -15,6 +15,7 @@ import java.util.Date;
 /**
  * The Story entity  represents the content of a story. This can be a Story, a nano story or a poem.
  * It also contains the author of said story and the topic and subtopic
+ *
  * @author Julio Patricio Da Silva (jpsilva) Brais Domínguez Álvarez (bdalvarez)
  */
 @Entity
@@ -40,28 +41,68 @@ public class Story implements Serializable {
 
     private String author;
     private boolean published;
+
     public Story() {
     }
 
-    public Story(int id, Date date, String title, String content, Genre genre, Theme mainTheme, Theme secondaryTheme, String author,boolean published) throws IllegalArgumentException {
-        this.id = id;
-        this.date = date;
-        this.title = title;
+    public Story(int id, Date date, String title, String content, Genre genre, Theme mainTheme, Theme secondaryTheme, String author, boolean published) throws IllegalArgumentException, NullPointerException {
+
+        if (date == null) {
+            throw new NullPointerException("Error: Content for a date cannot be null");
+        }
+
+        if (title.length() > 70) {
+            throw new IllegalArgumentException("Error: Content for a title cannot exceed 70 characters");
+        } else if (title.length() == 0) {
+            throw new IllegalArgumentException("Error: Content for a title cannot be empty");
+        } else if (title == null) {
+            throw new NullPointerException("Error: Content for a title cannot be null");
+        }
+
+        if (content.length() == 0) {
+            throw new IllegalArgumentException("Error: Content for a content cannot be empty");
+        } else if (content == null) {
+            throw new NullPointerException("Error: Content for a content cannot be null");
+        }
+
+        if (genre == null) {
+            throw new NullPointerException("Error: Content for a genre cannot be null");
+        }
 
         switch (genre) {
             case STORY:
-                if(content.length() >1000)
+                if (content.length() > 1000)
                     throw new IllegalArgumentException("Error: Content for a story cannot exceed 1000 characters");
                 break;
             case POETRY:
-                if(content.length() >500)
+                if (content.length() > 500)
                     throw new IllegalArgumentException("Error: Content for a poetry cannot exceed 500 characters");
                 break;
             case NANOSTORY:
-                if(content.length() >150)
-                    throw new IllegalArgumentException("Error: Content for a nanostory cannot exceed 150 characters");
+                if (content.length() > 150)
+                    throw new IllegalArgumentException("Error: Content for a nano-story cannot exceed 150 characters");
                 break;
         }
+
+        if (mainTheme == null) {
+            throw new NullPointerException("Error: Content for a mainTheme cannot be null");
+        }
+
+        if (secondaryTheme == null) {
+            throw new NullPointerException("Error: Content for a secondaryTheme cannot be null");
+        }
+
+        if (author.length() > 255) {
+            throw new IllegalArgumentException("Error: Content for a author cannot exceed 255 characters");
+        } else if (author.length() == 0) {
+            throw new IllegalArgumentException("Error: Content for a author cannot be empty");
+        } else if (author == null) {
+            throw new NullPointerException("Error: Content for a author cannot be null");
+        }
+
+        this.id = id;
+        this.date = date;
+        this.title = title;
         this.content = content;
         this.genre = genre;
         this.mainTheme = mainTheme;
--- domain/src/test/java/es/uvigo/esei/dgss/teama/microstories/domain/entities/StoryEntityTest.java
+++ domain/src/test/java/es/uvigo/esei/dgss/teama/microstories/domain/entities/StoryEntityTest.java
@@ -2,6 +2,7 @@ package es.uvigo.esei.dgss.teama.microstories.domain.entities;
 
 
 import org.junit.Test;
+
 import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -39,7 +40,7 @@ public class StoryEntityTest {
         assertThat(localStory.getAuthor(), is(equalTo(author)));
         assertThat(localStory.isPublished(), is(equalTo(published)));
     }
- 
+
 
     @Test(expected = IllegalArgumentException.class)
     public void testStoryLengthNanoStory() throws IllegalArgumentException {
@@ -57,7 +58,7 @@ public class StoryEntityTest {
 
     }
 
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void testStoryLengthPoetryStory() throws IllegalArgumentException {
         final int id = 1;
@@ -72,7 +73,7 @@ public class StoryEntityTest {
 
         Story localStory = new Story(id, date, title, content, genre, mainTheme, secondaryTheme, author, published);
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void testStoryLengthStory() throws IllegalArgumentException {
         final int id = 1;
@@ -86,5 +87,181 @@ public class StoryEntityTest {
         final boolean published = true;
 
         Story localStory = new Story(id, date, title, content, genre, mainTheme, secondaryTheme, author, published);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testStoryNullTitle() throws NullPointerException {
+        final int id = 1;
+        final String title = null;
+        final String content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non eros nec purus scelerisque varius vitae non mauris. Nunc malesuada efficitur quam, quis rhoncus leo efficitur lacinia. Proin enim quam, faucibus nec leo eget, euismod tincidunt risus. Suspendisse tristique et tortor vitae feugiat. Nulla tristique semper diam at molestie. In cursus tempor sem quis ultrices. Vestibulum vestibulum purus non pulvinar aliquam. Morbi sit amet ipsum eu ex pulvinar accumsan at at dolor. Nunc eu auctor orci. Sed maximus metus dui, ut commodo nisi dignissim sit amet. Sed ut nunc elementum, gravida nibh at, volutpat dui. Quisque fringilla mi sapien, ac blandit risus tincidunt sit amet. Aenean pulvinar, felis a pellentesque aliquet, mi erat scelerisque dui, pellentesque molestie est ex non lectus. Curabitur rhoncus libero sem, id malesuada arcu volutpat a. Fusce feugiat enim in justo ornare, non condimentum magna dignissim. Morbi neque tellus, viverra sit amet blandit eu, commodo at nisl. ";
+        final Genre genre = Genre.NANOSTORY;
+        final Theme mainTheme = Theme.ADVENTURE;
+        final Theme secondaryTheme = Theme.SUSPENSE;
+        final String author = "Antonio";
+        final Date date = new Date();
+        final boolean published = true;
+
+        new Story(id, date, title, content, genre, mainTheme, secondaryTheme, author, published);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testStoryLengthTitleMore70() throws IllegalArgumentException {
+        final int id = 1;
+        final String title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non gravida lacus, et porta ligula. Integer id nisl ac risus fermentum malesuada nec id est. Maecenas quis tristique purus, in lacinia arcu. Cras placerat arcu erat, non sodales nisi blandit ut. Fusce ac magna posuere, dapibus nulla sit amet, pulvinar turpis. Morbi vitae diam sit amet felis pellentesque pharetra. Curabitur sollicitudin suscipit gravida. Duis vehicula nisi nunc. Pellentesque sodales bibendum sem. ";
+        final String content = "Lorem ipsum dolor sit amet, consectetud tincidunt";
+        final Genre genre = Genre.NANOSTORY;
+        final Theme mainTheme = Theme.ADVENTURE;
+        final Theme secondaryTheme = Theme.SUSPENSE;
+        final String author = "Antonio";
+        final Date date = new Date();
+        final boolean published = true;
+
+        new Story(id, date, title, content, genre, mainTheme, secondaryTheme, author, published);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testStoryEmptyTitle() throws IllegalArgumentException {
+        final int id = 1;
+        final String title = "";
+        final String content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non eros nec purus scelerisque varius vitae non mauris. Nunc malesuada efficitur quam, quis rhoncus leo efficitur lacinia. Proin enim quam, faucibus nec leo eget, euismod tincidunt risus. Suspendisse tristique et tortor vitae feugiat. Nulla tristique semper diam at molestie. In cursus tempor sem quis ultrices. Vestibulum vestibulum purus non pulvinar aliquam. Morbi sit amet ipsum eu ex pulvinar accumsan at at dolor. Nunc eu auctor orci. Sed maximus metus dui, ut commodo nisi dignissim sit amet. Sed ut nunc elementum, gravida nibh at, volutpat dui. Quisque fringilla mi sapien, ac blandit risus tincidunt sit amet. Aenean pulvinar, felis a pellentesque aliquet, mi erat scelerisque dui, pellentesque molestie est ex non lectus. Curabitur rhoncus libero sem, id malesuada arcu volutpat a. Fusce feugiat enim in justo ornare, non condimentum magna dignissim. Morbi neque tellus, viverra sit amet blandit eu, commodo at nisl. ";
+        final Genre genre = Genre.NANOSTORY;
+        final Theme mainTheme = Theme.ADVENTURE;
+        final Theme secondaryTheme = Theme.SUSPENSE;
+        final String author = "Antonio";
+        final Date date = new Date();
+        final boolean published = true;
+
+        new Story(id, date, title, content, genre, mainTheme, secondaryTheme, author, published);
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testStoryNullAuthor() throws NullPointerException {
+        final int id = 1;
+        final String title = "Title test";
+        final String content = "Lneque tellus, viverra sit amet blandit eu, commodo at nisl. ";
+        final Genre genre = Genre.NANOSTORY;
+        final Theme mainTheme = Theme.ADVENTURE;
+        final Theme secondaryTheme = Theme.SUSPENSE;
+        final String author = null;
+        final Date date = new Date();
+        final boolean published = true;
+
+        new Story(id, date, title, content, genre, mainTheme, secondaryTheme, author, published);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testStoryEmptyAuthor() throws IllegalArgumentException {
+        final int id = 1;
+        final String title = "";
+        final String content = "Lneque tellus, viverra sit amet blandit eu, commodo at nisl. ";
+        final Genre genre = Genre.NANOSTORY;
+        final Theme mainTheme = Theme.ADVENTURE;
+        final Theme secondaryTheme = Theme.SUSPENSE;
+        final String author = "";
+        final Date date = new Date();
+        final boolean published = true;
+
+        new Story(id, date, title, content, genre, mainTheme, secondaryTheme, author, published);
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testStoryNullContent() throws NullPointerException {
+        final int id = 1;
+        final String title = "Title test";
+        final String content = null;
+        final Genre genre = Genre.NANOSTORY;
+        final Theme mainTheme = Theme.ADVENTURE;
+        final Theme secondaryTheme = Theme.SUSPENSE;
+        final String author = "Pepe";
+        final Date date = new Date();
+        final boolean published = true;
+
+        new Story(id, date, title, content, genre, mainTheme, secondaryTheme, author, published);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testStoryEmptyContent() throws IllegalArgumentException {
+        final int id = 1;
+        final String title = "";
+        final String content = "";
+        final Genre genre = Genre.NANOSTORY;
+        final Theme mainTheme = Theme.ADVENTURE;
+        final Theme secondaryTheme = Theme.SUSPENSE;
+        final String author = "Pepe";
+        final Date date = new Date();
+        final boolean published = true;
+
+        new Story(id, date, title, content, genre, mainTheme, secondaryTheme, author, published);
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testStoryNullMainTheme() throws NullPointerException {
+        final int id = 1;
+        final String title = "Title test";
+        final String content = "Lneque tellus, viverra sit amet blandit eu, commodo at nisl. ";
+        final Genre genre = Genre.NANOSTORY;
+        final Theme mainTheme = null;
+        final Theme secondaryTheme = Theme.SUSPENSE;
+        final String author = "Pepe";
+        final Date date = new Date();
+        final boolean published = true;
+
+        new Story(id, date, title, content, genre, mainTheme, secondaryTheme, author, published);
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testStoryNullSecondaryTheme() throws NullPointerException {
+        final int id = 1;
+        final String title = "Title test";
+        final String content = "Lneque tellus, viverra sit amet blandit eu, commodo at nisl. ";
+        final Genre genre = Genre.NANOSTORY;
+        final Theme mainTheme = Theme.ADVENTURE;
+        final Theme secondaryTheme = null;
+        final String author = "Pepe";
+        final Date date = new Date();
+        final boolean published = true;
+
+        new Story(id, date, title, content, genre, mainTheme, secondaryTheme, author, published);
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testStoryNullDate() throws NullPointerException {
+        final int id = 1;
+        final String title = "Title test";
+        final String content = "Lneque tellus, viverra sit amet blandit eu, commodo at nisl. ";
+        final Genre genre = Genre.NANOSTORY;
+        final Theme mainTheme = Theme.HISTORIC;
+        final Theme secondaryTheme = Theme.SUSPENSE;
+        final String author = "Pepe";
+        final Date date = null;
+        final boolean published = true;
+
+        new Story(id, date, title, content, genre, mainTheme, secondaryTheme, author, published);
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testStoryNullGenre() throws NullPointerException {
+        final int id = 1;
+        final String title = "Title test";
+        final String content = "Lneque tellus, viverra sit amet blandit eu, commodo at nisl. ";
+        final Genre genre = null;
+        final Theme mainTheme = Theme.HISTORIC;
+        final Theme secondaryTheme = Theme.SUSPENSE;
+        final String author = "Pepe";
+        final Date date = new Date();
+        final boolean published = true;
+
+        new Story(id, date, title, content, genre, mainTheme, secondaryTheme, author, published);
+
     }
 }
