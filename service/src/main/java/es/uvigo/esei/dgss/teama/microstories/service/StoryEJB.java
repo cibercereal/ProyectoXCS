a61a3907b596e56e1f3acfaa92a92cd610bc7528
Álvaro Suárez <alvarosuafe@gmail.com>
2022-11-28 19:45:20 +0100

    Removes @PermitAll annotation in StoryEJB methods
    The  @PermitAll annotation is in the class so not makes sense
    use it in the methods of the class also.

--- service/src/main/java/es/uvigo/esei/dgss/teama/microstories/service/StoryEJB.java
+++ service/src/main/java/es/uvigo/esei/dgss/teama/microstories/service/StoryEJB.java
@@ -18,14 +18,12 @@ public class StoryEJB {
     @PersistenceContext
     private EntityManager em;
 
-    @PermitAll
     public List<Story> getRecentStories() {
         final int LIMIT_STORIES = 6;
         return em.createQuery("SELECT s FROM Story s WHERE s.published IS TRUE ORDER BY s.date DESC, s.id ASC",
                 Story.class).setMaxResults(LIMIT_STORIES).getResultList();
     }
 
-    @PermitAll
     public Story getById(final int id) {
         return em.find(Story.class, id);
     }
